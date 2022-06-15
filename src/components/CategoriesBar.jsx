741a649b23d5f806670d63fde8b8cf8b832cad10
Mislav Petrina <petrinamislav@gmail.com>
2022-06-15 15:05:49 +0200

    made a categories bar and styled it, added sass variables

---
+++ src/components/CategoriesBar.jsx
@@ -0,0 +1,70 @@
+import { useState } from 'react';
+
+import './styles/categoriesBar.scss';
+
+export const CategoriesBar = () => {
+
+    const [isActive, setActive] = useState('news');
+   
+    const toggleActive = (e) => {
+        const { title } = e.currentTarget;
+
+        if (isActive == title) {
+            setActive(title);
+        } 
+        else {
+            setActive(title);
+        }
+    }
+
+    return (
+        <section className='categories-section'>
+            <div className="categories-container">
+                <ul>
+                    <li 
+                        className={isActive == 'news' ? 'active-list' : 'news-list'}
+                        title='news'
+                        onClick={toggleActive}
+                    >
+                        <a>NEWS</a>
+                    </li>
+                    <li 
+                        className={isActive == 'business' ? 'active-list' : 'business-list'}
+                        title='business' 
+                        onClick={toggleActive}
+                    >
+                        <a>BUSINESS</a>
+                    </li>
+                    <li 
+                        className={isActive == 'sport' ? 'active-list' : 'sport-list'}
+                        title='sport'
+                        onClick={toggleActive}
+                    >
+                        <a>SPORT</a>
+                    </li>
+                    <li 
+                        className={isActive == 'life' ? 'active-list' : 'life-list'}
+                        title='life'
+                        onClick={toggleActive}
+                    >
+                        <a>LIFE</a>
+                    </li>
+                    <li 
+                        className={isActive == 'tech' ? 'active-list' : 'tech-list'}
+                        title='tech'
+                        onClick={toggleActive}
+                    >
+                        <a>TECH</a>
+                    </li>
+                    <li 
+                        className={isActive == 'travel' ? 'active-list' : 'travel-list'}
+                        title='travel'
+                        onClick={toggleActive}
+                    >
+                        <a>TRAVEL</a>
+                    </li>
+                </ul>
+            </div>
+        </section>
+    )
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,10 @@
-import { Header } from "../components"
+import { Header, CategoriesBar } from "../components"
 
 export const Home = () => {
   return (
     <>
       <Header />
+      <CategoriesBar />
     </>
   )
 }
